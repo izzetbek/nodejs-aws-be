573b18bba31721ecd2622748c2b671f2889fa367
Izzat_Fatullayev <Izzat_Fatullayev@epam.com>
2020-11-16 12:47:13 +0300

    fix: refactored importFileParser handler

--- src/import-service/src/importFileParser.js
+++ src/import-service/src/importFileParser.js
@@ -2,32 +2,26 @@ import AWS from "aws-sdk";
 import csv from "csv-parser";
 import response from "../../utils/response";
 
-export const importFileParser = async event => {
+export const importFileParser = event => {
     const s3 = new AWS.S3({ region: 'eu-west-1' });
     const Bucket = 'nodejs-aws-static';
     const errors = [];
     for (const record of event.Records) {
         const Key = record.s3.object.key;
 
-        try {
-            const s3Obj = await s3.getObject({ Bucket, Key }).promise();
+        s3.getObject({ Bucket, Key }).createReadStream()
+            .pipe(csv())
+            .on('data', data => console.log(data))
+            .on('end', async () => {
+                await s3.copyObject({
+                    Bucket,
+                    Key: Key.replace('uploaded', 'parsed'),
+                    CopySource: `${Bucket}/${Key}`
+                }).promise();
 
-            const results = [];
-            s3Obj.createReadStream().pipe(csv())
-                .on('data', data => results.push(data))
-                .on('error', error => errors.push(error))
-                .on('end', () => console.log(results));
-
-            await s3.copyObject({
-                Bucket,
-                Key: Key.replace('uploaded', 'parsed'),
-                CopySource: `${Bucket}/${Key}`
-            }).promise();
-
-            await s3.deleteObject({ Bucket, Key }).promise();
-        } catch (e) {
-            errors.push(e);
-        }
+                await s3.deleteObject({ Bucket, Key }).promise();
+            })
+            .on('error', error => errors.push(error));
     }
 
     if (errors.length) {
